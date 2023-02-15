93c0a933471dc5f59ffb7b36cfb865f263cc91a5
Kushagra Saxena <devkushagra2002@gmail.com>
2023-02-15 11:48:28 +0530

    First Commit

---
+++ precache-manifest.dccd519b22e284729025d40328e33b6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "94574c4e9db58eb1412f",
+    "url": "/static/css/main.7cd476d8.chunk.css"
+  },
+  {
+    "revision": "94574c4e9db58eb1412f",
+    "url": "/static/js/main.94574c4e.chunk.js"
+  },
+  {
+    "revision": "7fc5741dfb5d402e95fa",
+    "url": "/static/js/1.7fc5741d.chunk.js"
+  },
+  {
+    "revision": "f91a93f2914e909f3e71",
+    "url": "/static/js/2.f91a93f2.chunk.js"
+  },
+  {
+    "revision": "72cba3a7707ecab0fd5b",
+    "url": "/static/js/runtime~main.72cba3a7.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "ac70a8b9af32ea7943285aeddb3d935b",
+    "url": "/index.html"
+  }
+];
